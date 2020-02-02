0f815635a8b88d21ac296fd67c3147b14d726ea8
michael-j-taylor <michaeljtaylor15@gmail.com>
2020-02-01 23:55:20 -0600

    Data now displays on screen

--- android_applications/test_mouse_app/app/src/main/java/com/example/testmouseapp/MainActivity.java
+++ android_applications/test_mouse_app/app/src/main/java/com/example/testmouseapp/MainActivity.java
@@ -1,6 +1,5 @@
 package com.example.testmouseapp;
 
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Context;
@@ -10,6 +9,7 @@ import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity implements SensorEventListener {
 
@@ -18,7 +18,12 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
     private SensorManager sensorManager;
     Sensor accelerometer;
 
-    private boolean dir = false;  //determine when x value changes direction
+    //maximum and minimum acceleration values measured
+    float xmax = 0;
+    float xmin = 0;
+    float ymax = 0;
+    float ymin = 0;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,20 +36,40 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
         sensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
 
         //get sensor (accelerometer in this case)
-        accelerometer = sensorManager.getDefaultSensor(Sensor.TYPE_LINEAR_ACCELERATION);
+        accelerometer = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
 
         //setup listener
-        sensorManager.registerListener(this, accelerometer, SensorManager.SENSOR_DELAY_NORMAL);  //can be changed to faster modes
+        sensorManager.registerListener(this, accelerometer, SensorManager.SENSOR_DELAY_NORMAL);  //can be changed to different delays
 
         Log.d(TAG, "onCreate: Registered accelerometer listener");
 
 
     }
 
+    //on sensor value change, display X and Z values
     @Override
     public void onSensorChanged(SensorEvent event) {
-        if (event.values[0] > 0.0254 || event.values[0] < -0.0254) {
-            Log.d(TAG, "onSensorChanged: X: " + event.values[0] + " Y: " + event.values[1] + " Z: " + event.values[2]);
+        if (event.values[0] > 0.0254 || event.values[0] < -0.0254) {  //attempt to ignore small values
+
+            if (event.values[0] > xmax) {xmax = event.values[0];}
+            if (event.values[0] < xmin) {xmin = event.values[0];}
+
+            if (event.values[1] > ymax) { ymax = event.values[1];}
+            if (event.values[1] < ymin) { ymin = event.values[1];}
+
+
+            TextView live_acceleration;
+            TextView max_acceleration;
+
+            live_acceleration = (TextView)findViewById(R.id.acceleration);
+            max_acceleration = (TextView)findViewById(R.id.maximums);
+
+            //Log.d(TAG, "onSensorChanged: X: " + event.values[0] + " Y: " + event.values[1] + " Z: " + event.values[2]);
+            String data_live = "X: " + event.values[0] + "\nY: " + event.values[1];
+            String data_max = "X Maximum: " + xmax + "\nX Minimum: " + xmin + "\n\nY Maximum: " + ymax + "\nY Minimum: " + ymin;
+
+            live_acceleration.setText(data_live);
+            max_acceleration.setText(data_max);
         }
     }
 
