1d8841529cb420195e24a346f842366f63a3eeff
michael-j-taylor <michaeljtaylor15@gmail.com>
2020-03-09 19:39:48 -0500

    Updated Calibrater and Filter classes

--- android_applications/test_mouse_app/app/src/main/java/com/example/testmouseapp/dataOperations/Filter.java
+++ android_applications/test_mouse_app/app/src/main/java/com/example/testmouseapp/dataOperations/Filter.java
@@ -1,25 +1,34 @@
 package com.example.testmouseapp.dataOperations;
 
+
+//calculates thresholds for x, y, and magnitude given offsets from a calibrated accelerometer and a percentage
 public class Filter {
 
-    private float xmax;
-    private float xmin;
-    private float ymax;
-    private float ymin;
+    public float y_threshold;
+    public float x_threshold;
+    public float magnitude_threshold;
 
-    public Filter(float xmax, float xmin, float ymax, float ymin) {
-        this.xmax = xmax;
-        this.xmin = xmin;
-        this.ymax = ymax;
-        this.ymin = ymin;
+    public Filter(float x_offset, float y_offset, double percent) {
+        this.x_threshold = (float) calculateThreshold(x_offset, percent);
+        this.y_threshold = (float) calculateThreshold(y_offset, percent);
+        this.magnitude_threshold = calculateMagnitudeThreshold(this.x_threshold, this.y_threshold);
     }
 
-    //return false if data is between specified maximun and minumum x and y values
-    public boolean filterData(float xdata, float ydata) {
-        if ( (xdata < xmax && xdata > xmin) || (ydata < ymax && ydata > ymin) ) {
-            return false;
+
+    //adjust given x or y offset by percent
+    private double calculateThreshold(float offset, double percent) {
+        if (offset > 0) {
+            return offset + (offset*percent);
         }
-        return true;
+        else if (offset < 0) {
+            return offset - (offset*percent);
+        }
+        else return offset;
     }
 
+
+    //calculate threshold for magnitude, using previously calculated x and y thresholds
+    private float calculateMagnitudeThreshold(float x_threshold, float y_threshold) {
+        return (float) Math.sqrt(Math.pow(x_threshold, 2) + Math.pow(y_threshold, 2));
+    }
 }
