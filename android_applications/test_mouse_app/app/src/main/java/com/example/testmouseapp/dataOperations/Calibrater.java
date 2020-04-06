a1ae188e229ab1ada6d0c622c791048f00607754
michael-j-taylor <michaeljtaylor15@gmail.com>
2020-04-06 14:59:14 -0500

    Basic presentation mode structure implemented

--- android_applications/test_mouse_app/app/src/main/java/com/example/testmouseapp/dataOperations/Calibrater.java
+++ android_applications/test_mouse_app/app/src/main/java/com/example/testmouseapp/dataOperations/Calibrater.java
@@ -1,15 +1,71 @@
 package com.example.testmouseapp.dataOperations;
 
-//used to calibrate acclerometer
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.Iterator;
+import java.util.List;
+import java.util.ListIterator;
+
+//used to calibrate accelerometer
 public class Calibrater {
 
-    private int num_readings;
+    private int total_readings;
+    private int num_readings = 0;
+    private float x_sum = 0;
+    private float y_sum = 0;
+    private float x_max_reading = 0;
+    private float y_max_reading = 0;
+
+    private ArrayList<Float> x_readings = new ArrayList<>();
+    private ArrayList<Float> y_readings = new ArrayList<>();
+
+    public float magnitude_offset = 0;
+    public float x_offset = 0;
+    public float y_offset = 0;
 
     public boolean calibrating = false;
 
-    public Calibrater(int num_readings) {
-        this.num_readings = num_readings;
+    public Calibrater(int total_readings) {
+        this.total_readings = total_readings;
     }
 
-    
+
+    public void addDataPoint(float reading_x, float reading_y) {
+        num_readings++;
+        this.x_sum += reading_x;
+        this.y_sum += reading_y;
+
+        this.x_readings.add(reading_x);
+        this.y_readings.add(reading_y);
+    }
+
+
+    public void calibrate(float reading_x, float reading_y) {
+
+        if (!calibrating) {
+            this.calibrating = true;
+        }
+
+        if (num_readings == total_readings) {
+
+            calculateOffsets();
+            this.calibrating = false;
+        }
+
+        else {
+            addDataPoint(reading_x, reading_y);
+        }
+    }
+
+
+    private void calculateOffsets() {
+
+        this.x_offset = this.x_sum/this.num_readings;
+        this.y_offset = this.y_sum/this.num_readings;
+        this.magnitude_offset = (float) Math.sqrt(Math.pow(this.x_offset, 2) + Math.pow(this.y_offset, 2));
+    }
 }
--- android_applications/test_mouse_app/app/src/main/java/com/example/testmouseapp/dataOperations/Filter.java
+++ android_applications/test_mouse_app/app/src/main/java/com/example/testmouseapp/dataOperations/Filter.java
@@ -1,25 +1,73 @@
 package com.example.testmouseapp.dataOperations;
 
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+//calculates thresholds for x, y, and magnitude given offsets from a calibrated accelerometer and a percentage
 public class Filter {
 
-    private float xmax;
-    private float xmin;
-    private float ymax;
-    private float ymin;
+    public float y_threshold;
+    public float x_threshold;
+    public float magnitude_threshold;
 
-    public Filter(float xmax, float xmin, float ymax, float ymin) {
-        this.xmax = xmax;
-        this.xmin = xmin;
-        this.ymax = ymax;
-        this.ymin = ymin;
+    private Calibrater calibrater;
+
+    public Filter(Calibrater calibrater, double percent) {
+
+        this.calibrater = calibrater;
+        this.magnitude_threshold = calculateMagnitudeThreshold(this.x_threshold, this.y_threshold);
     }
 
-    //return false if data is between specified maximun and minumum x and y values
-    public boolean filterData(float xdata, float ydata) {
-        if ( (xdata < xmax && xdata > xmin) || (ydata < ymax && ydata > ymin) ) {
-            return false;
+
+    //adjust given x or y offset by percent
+    /*
+    private double calculateThreshold(ArrayList<Float> readings, double percent) {
+
+        Collections.sort(readings);
+        float x_avg = this.calibrater.x_offset / readings.size();
+        float y_avg = this.calibrater.y_offset / readings.size();
+
+        if (x_avg > 0) {
+
         }
-        return true;
+        return avg_threshold + (avg_threshold*percent);
+    }
+     */
+
+
+    //calculate threshold for magnitude, using previously calculated x and y thresholds
+    private float calculateMagnitudeThreshold(float x_threshold, float y_threshold) {
+        return (float) Math.sqrt(Math.pow(x_threshold, 2) + Math.pow(y_threshold, 2));
+    }
+
+
+    public float averageTopReadings(ArrayList<Float> readings, int num_readings) {
+
+        //list of last 5 elements of readings List
+        List<Float> topReadings = readings.subList(readings.size() - num_readings, readings.size());
+
+        float sum = 0;
+        for (int i = 0; i < num_readings; i++) {
+            sum += topReadings.indexOf(i);
+        }
+
+        return sum/num_readings;
+    }
+
+
+    public float averageBottomReadings(ArrayList<Float> readings, int num_readings) {
+
+        //create list of bottom 5 readings
+        List<Float> bottomReadings = readings.subList(0, num_readings);
+
+        float sum = 0;
+        for (int i = 0; i < num_readings; i++) {
+            sum += bottomReadings.indexOf(i);
+        }
+
+        return sum/num_readings;
     }
 
 }
