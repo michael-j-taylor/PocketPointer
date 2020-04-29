f3ccfc532d3031bbf9b44787c8f9a9df4d4cc76d
RyanBAtk <ryanb.atk@gmail.com>
2020-04-29 16:27:44 -0500

    Revert "Mouse movement has been made smoother and more predictable. Commit to start working on sending data."
    This reverts commit f631d3c42ab77263150a61409f27a6df84bc2be6.

--- android_applications/test_mouse_app/app/src/main/java/com/example/testmouseapp/fragments/HomeFragment.java
+++ android_applications/test_mouse_app/app/src/main/java/com/example/testmouseapp/fragments/HomeFragment.java
@@ -55,8 +55,8 @@ public class HomeFragment extends Fragment implements SensorEventListener {
     private float ymax = 0;
     private float ymin = 0;
 
-    private MovingAverage movingAverage_X;
-    private MovingAverage movingAverage_Y;
+    private MovingAverage movingAverage_X = new MovingAverage(100);
+    private MovingAverage movingAverage_Y = new MovingAverage(100);
 
     //printed accelerometer values
     private float accel_x, prev_accel_x, raw_x;
@@ -65,8 +65,6 @@ public class HomeFragment extends Fragment implements SensorEventListener {
     private long currentTime;
 
     private final int polling_rate = 60; //in Hz
-    private int twa = 0; //ticks without acceleration
-    private float friction_coefficient = .82f;
     private float time;
 
     private Calibrater calibrater;
@@ -91,9 +89,13 @@ public class HomeFragment extends Fragment implements SensorEventListener {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         time = 1.f/polling_rate;
-        calibrater = new Calibrater(50);
-        movingAverage_X = new MovingAverage(50);
-        movingAverage_Y = new MovingAverage(50);
+        calibrater = new Calibrater(100);
+        /*Button calibrate = view.findViewById(R.id.calibrate);
+        calibrate.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                calibrater.calibrating = true;
+            }});*/
+
     }
 
     @Override
@@ -242,25 +244,13 @@ public class HomeFragment extends Fragment implements SensorEventListener {
                 //calculate current value via moving average
                 accel_x = movingAverage_X.calculateAverage() - calibrater.x_offset;
                 accel_y = movingAverage_Y.calculateAverage() - calibrater.y_offset;
-                float average_calibrated_magnitude = (float) Math.sqrt(Math.pow(accel_x, 2) + Math.pow(accel_y, 2));
-                if (average_calibrated_magnitude < calibrater.magnitude_threshold)
+                if (calibrated_magnitude < calibrater.magnitude_threshold)
                 {
                     accel_x = 0;
                     accel_y = 0;
-                    twa++;
-                    if (twa >= 3) {
-                        x_vel *= friction_coefficient;
-                        y_vel *= friction_coefficient;
-                        float vel_mag = (float) Math.sqrt(x_vel * x_vel + y_vel * y_vel);
-                        if (vel_mag < .0001f) {
-                            x_vel = 0;
-                            y_vel = 0;
-                        }
-                    }
                 }
-                else
-                    twa = 0;
                 //Log.d(TAG, event.values[0] + " " + event.values[1]);//Log.d(TAG, val_x + " " + val_y);
+
                 //Log.d(TAG, "raw magnitude: " + raw_magnitude + " vs adjusted " + magnitude + "vs thresh " + calibrater.magnitude_threshold);
 
                 //calculate jerk
@@ -279,15 +269,29 @@ public class HomeFragment extends Fragment implements SensorEventListener {
 
                 String data_live = "X: " + String.format("%.3f", x_pos) + "\nY: " + String.format("%.3f", y_pos) + "\nax: " + accel_x +"\nay: " + accel_y +
                         "\ncx: " + calibrater.x_offset + "\ncy: " + calibrater.y_offset + "\nrx: " + String.format("%.5f", raw_x) +
-                        "\nry: " + String.format("%.5f", raw_y) + "\nvx: " + String.format("%.5f", x_vel) +
-                        "\nvy: " + String.format("%.5f", y_vel);
+                        "\nry: " + String.format("%.5f", raw_y);
+                String data_max = "X Maximum: " +
+                        String.format("%.3f", xmax) + "\nX Minimum: " +
+                        String.format("%.3f", xmin) + "\n\nY Maximum: " +
+                        String.format("%.3f", ymax) + "\nY Minimum: " +
+                        String.format("%.3f", ymin);
 
                 live_acceleration.setText(data_live);
                 //max_acceleration.setText(data_max);
                 //position.setText("Position: " + String.format("%.3f",x_pos) + ", " + String.format("%.3f",y_pos));
                 startTime = Calendar.getInstance().getTimeInMillis();
             }
-        //there was an else statement here, but it was basically empty
+            else {
+                //String data_live = "X: " + 0 + "\nY: " + 0;
+                String data_max = "X Maximum: " +
+                        String.format("%.3f", xmax) + "\nX Minimum: " +
+                        String.format("%.3f", xmin) + "\n\nY Maximum: " +
+                        String.format("%.3f", ymax) + "\nY Minimum: " +
+                        String.format("%.3f", ymin);
+
+                String data_live = "X: " + accel_x + "\nY: " + accel_y;
+                //live_acceleration.setText(data_live);
+            }
         }
     }
 
