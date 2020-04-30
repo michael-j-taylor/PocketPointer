41496f9b9d98d2734c785e1e3e70eddbeae9d2a9
jandrooos <57966275+jandrooos@users.noreply.github.com>
2020-04-29 23:42:35 -0500

    looking at JFrame

--- windows_applications/src/Driver/Receiver.java
+++ windows_applications/src/Driver/Receiver.java
@@ -2,6 +2,7 @@ package Driver;
 
 import javax.bluetooth.BluetoothStateException;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
 
 import Bluetooth.BluetoothServer;
@@ -19,6 +20,10 @@ public class Receiver extends JPanel
 
     public static void main(String args[])
     {
+    	
+    	JFrame frame = new JFrame(gc);
+        frameSetUp(frame);
+    	
     	BluetoothServer server = new BluetoothServer();
     	try {
     		server.openServer();
@@ -32,12 +37,8 @@ public class Receiver extends JPanel
     			System.out.println("Exception from openServer:\n" + e + e.getMessage() + "\n");
     	}
     	
-//        JFrame frame = new JFrame(gc);
-//        frame.setTitle("PocketPointer Receiver");
-//        frame.setSize(800, 600);
-//        frame.setVisible(true);
-//        frame.setResizable(false);
-//        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    	
+        
         //TODO: initialize components. pass frame to method
 //        robotTest();
         //while(true)
@@ -74,8 +75,22 @@ public class Receiver extends JPanel
         }
 
         // Create an instance of Robot class
-
-
+       
+    }
+    
+    public void paint(Graphics g) {
+    	g.drawString("Hello world", 10, 10);
+    }
+    
+    public static void frameSetUp(JFrame frame) {
+    	    	
+    	frame.setTitle("PocketPointer Receiver");
+        frame.setSize(800, 600);
+        frame.setVisible(true);
+        frame.setResizable(false);
+        //frame.getContentPane().setBackground(Color.DARK_GRAY.darker());
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+                    
         
     }
 }
