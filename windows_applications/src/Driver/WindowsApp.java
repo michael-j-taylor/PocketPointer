377f426d9a26ba963684f2c4a430e4540bea03cb
jandrooos <57966275+jandrooos@users.noreply.github.com>
2020-05-01 15:00:11 -0500

    Scrapped the menu idea. All on one screen.
    TODO:
    -call bt connect when button is pressed
    -output connection progress
    -make list of devices

--- windows_applications/src/Driver/WindowsApp.java
+++ windows_applications/src/Driver/WindowsApp.java
@@ -1,35 +1,25 @@
 package Driver;
 
 import javax.swing.*;
-import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 public class WindowsApp extends JFrame {
-    private JButton connect;
-    private JPanel conningPanel;
-    private JMenuBar bar;
-    private JMenu menu;
-    private JMenuItem connedNavi;
-    private JMenuItem conningNavi;
-    private JMenuItem exitWindow;
+    private JPanel mainPanel;
+    private JButton connectButton;
+    private JList deviceList;
+    private JLabel connectingOutput;
 
     public WindowsApp() {
-        add(conningPanel);
+        super("PocketPointer Receiver");
 
-        setTitle("PocketPointer Receiver");
-        setSize(600, 450);
+        setSize(550, 400);
         setResizable(false);
-        getContentPane().setBackground(Color.DARK_GRAY.darker());
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        setVisible(true);
 
-        exitWindow.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                System.exit(0);
-            }
-        });
+        getContentPane().add(mainPanel);
+
+        setVisible(true);
     }
 
     private void createUIComponents() {
