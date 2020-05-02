e16306e43225d11d6925056a647a2be7805e9753
bdonet <brjd345@gmail.com>
2020-05-02 15:23:00 -0500

    Implemented message send for touchpad coordinates

--- android_applications/test_mouse_app/app/src/main/java/com/example/testmouseapp/dataOperations/pointerTracker.java
+++ android_applications/test_mouse_app/app/src/main/java/com/example/testmouseapp/dataOperations/pointerTracker.java
@@ -9,32 +9,51 @@ public class pointerTracker {
 
     private static final String TAG = "pointerTracker";
 
-    private float x, y;
+    private double x1;
+    private double y1;
+    private double dx;
+    private double dy;
 
     public pointerTracker() {
-        this.x = 0;
-        this.y = 0;
+        x1 = 0;
+        y1 = 0;
+        dx = 0;
+        dy = 0;
     }
 
     public void setMouseCoordinates(MotionEvent event) {
+        double x2 = x1;
+        double y2 = y1;
 
         switch (event.getAction()) {
 
             case MotionEvent.ACTION_DOWN: {
-                this.x = event.getX();
-                this.y = event.getY();
-                Log.d(TAG, "initialized with x: " + this.x + " | y: " + this.y);
+                this.x1 = event.getX();
+                this.y1 = event.getY();
+                x2 = x1;
+                y2 = y1;
+                Log.d(TAG, "initialized with x: " + this.x1 + " | y: " + this.y1);
                 break;
             }
 
             case MotionEvent.ACTION_MOVE: {
-                this.x = event.getX();
-                this.y = event.getY();
-                Log.d(TAG, "x: " + this.x + " | y: " + this.y);
+                this.x1 = event.getX();
+                this.y1 = event.getY();
+                Log.d(TAG, "x: " + this.x1 + " | y: " + this.y1);
                 break;
             }
+
         }
+
+        dx = x1 - x2;
+        dy = y1 - y2;
     }
 
+    public double getDeltaX() {
+        return dx;
+    }
 
+    public double getDeltaY() {
+        return dy;
+    }
 }
